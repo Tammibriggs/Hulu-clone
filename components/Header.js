cb3db62907a806b7a5272924f47ade2fb3a544fe
Tammibriggs <Godsfirstbriggs@gmail.com>
2021-11-08 02:51:10 +0100

    gave the header title different names

--- components/Header.js
+++ components/Header.js
@@ -14,11 +14,11 @@ function Header() {
     <header className='flex flex-col sm:flex-row m-5 justify-between items-center'>
       <div className='flex flex-grow justify-evenly max-w-2xl h-auto'>
         <HeaderItem title='HOME' Icon={HomeIcon} />
-        <HeaderItem title='HOME' Icon={LightningBoltIcon} />
-        <HeaderItem title='HOME' Icon={BadgeCheckIcon} />
-        <HeaderItem title='HOME' Icon={CollectionIcon} />
-        <HeaderItem title='HOME' Icon={SearchIcon} />
-        <HeaderItem title='HOME' Icon={UserIcon} />
+        <HeaderItem title='TRENDING' Icon={LightningBoltIcon} />
+        <HeaderItem title='VERIFIED' Icon={BadgeCheckIcon} />
+        <HeaderItem title='COLLECTIONS' Icon={CollectionIcon} />
+        <HeaderItem title='SEARCH' Icon={SearchIcon} />
+        <HeaderItem title='ACCOUNT' Icon={UserIcon} />
       </div>
       <Image
         className='object-contain'
